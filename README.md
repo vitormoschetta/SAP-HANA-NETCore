d4095b6c298ec4962985e1a21af1bb5795e2de4b
Vitor <vfarias@bionexo.com>
2021-07-21 19:12:33 -0400

    add project

---
+++ README.md
@@ -0,0 +1,49 @@
+# Quick Start
+
+
+### Up Database:
+
+```
+docker-compose up -d --build
+```
+
+
+
+### Install SAP HANA Client:
+
+[Instalação do Client SAP Hana](https://bionexo.atlassian.net/wiki/spaces/IE/pages/2285240397/Instala+o+do+Client+SAP+Hana)
+
+
+##### Path installation:
+
+1. Windows:
+
+```
+C:\SapHanaBionexo
+```
+
+
+2. Linux:
+
+```
+/SapHanaBionexo
+```
+
+
+##### Note:
+
+After installation there must be a folder named 'dotnetcore' in the directory given above
+
+example: /SapHanaBionexo/dotnetcore 
+
+
+
+
+
+
+
+
+#### Execute app:
+```
+dotnet run
+```
---
+++ src/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using Sap.Data.Hana;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SapHanaSetEnvironment();
+
+            var server = "localhost:39017";
+            var user = "system";
+            var password = "Password854*";
+            var others = "encrypt=true;sslValidateCertificate=false";
+
+            try
+            {
+                using (var conn = new HanaConnection(string.Format("Server={0};UID={1};PWD={2}; {3}", server, user, password, others)))
+                {
+                    conn.Open();
+                    Console.WriteLine("Connected to " + server);
+         
+                    var query = "SELECT * FROM sys.AFL_AREAS_";
+                    using (var cmd = new HanaCommand(query, conn))
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        Console.WriteLine("Query result:");
+
+                        // Print column names
+                        var sbCol = new StringBuilder();
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {                            
+                            sbCol.Append(reader.GetName(i).PadRight(20));
+                        }
+                        Console.WriteLine(sbCol.ToString());
+
+                        // Print rows
+                        while (reader.Read())
+                        {
+                            var sbRow = new StringBuilder();
+
+                            // Print items
+                            for (var i = 0; i < reader.FieldCount; i++)
+                            {                                
+                                sbRow.Append(reader[i].ToString().PadRight(20));
+                            }
+                            Console.WriteLine(sbRow.ToString());
+                        }
+                        conn.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error - " + ex.Message);
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void SapHanaSetEnvironment()
+        {
+            var SapHanaEnv = Environment.GetEnvironmentVariable("HDBDOTNETCORE");
+
+            if (SapHanaEnv == null)
+            {
+                Environment.SetEnvironmentVariable("HDBDOTNETCORE", "/SapHanaBionexo/dotnetcore");
+            }
+        }
+    }
+}
